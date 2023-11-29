de44815b5729cdd5fed3c4ac444552a160e27e7b
chiahanlu <ducklyu0301@gmail.com>
2023-11-29 13:34:26 +0800

    black format

--- example/exp2_following.py
+++ example/exp2_following.py
@@ -19,18 +19,19 @@ LOAD_TIME = "1124134231"
 NUM_TRAJECTORIES = 500
 DDJC_THRES = (40, 50, 60, 70, 80, 90, 100)
 
+
 def path_following(test_pafik: bool, test_ikflow: bool):
     solver = PathFollower(solver_param=DEFAULT_SOLVER_PARAM_M7_NORM)
 
     J, P = solver.sample_Jtraj_Ppath(load_time=LOAD_TIME, num_steps=20)
-    
+
     if test_pafik:
         begin_time = time()
         J_hat, l2_errs, ang_errs, mjac_arr, ddjc = solver.solve_path(
             J, P, num_traj=NUM_TRAJECTORIES, return_numpy=True, return_evaluation=True
         )
         end_time = time()
-        
+
         df = pd.DataFrame(
             {
                 "l2_errs": l2_errs,
@@ -45,15 +46,17 @@ def path_following(test_pafik: bool, test_ikflow: bool):
             sr = df.query(f"ddjc < {thres}")["ddjc"].count() / df.shape[0]
             table[i] = (thres, sr, round(1 / sr)) if sr != 0 else (thres, sr, np.inf)
         print(tabulate(table, headers=["ddjc", "success rate", "min num sols"]))
-        print(f"avg_inference_time: {round((end_time - begin_time) / NUM_TRAJECTORIES, 3)}")
+        print(
+            f"avg_inference_time: {round((end_time - begin_time) / NUM_TRAJECTORIES, 3)}"
+        )
         print(df.describe())
-    
+
     if test_ikflow:
         set_seed()
 
         # Build IKFlowSolver and set weights
         ik_solver, _ = get_ik_solver("panda__full__lp191_5.25m")
-        
+
         F_l2_err = np.empty((NUM_TRAJECTORIES,))
         F_ang_err = np.empty((NUM_TRAJECTORIES,))
         F_mjac = np.empty((NUM_TRAJECTORIES,))
@@ -84,14 +87,17 @@ def path_following(test_pafik: bool, test_ikflow: bool):
                 "ddjc": np.rad2deg(F_ddjc),
             }
         )
-        
+
         table = np.empty((len(DDJC_THRES), 3))
         for i, thres in enumerate(DDJC_THRES):
             sr = F_df.query(f"ddjc < {thres}")["ddjc"].count() / F_df.shape[0]
             table[i] = (thres, sr, round(1 / sr)) if sr != 0 else (thres, sr, np.inf)
         print(tabulate(table, headers=["ddjc", "success rate", "min num sols"]))
-        print(f"avg_inference_time: {round((end_time - begin_time) / NUM_TRAJECTORIES, 3)}")
+        print(
+            f"avg_inference_time: {round((end_time - begin_time) / NUM_TRAJECTORIES, 3)}"
+        )
         print(F_df.describe())
-        
+
+
 if __name__ == "__main__":
     path_following(test_pafik=TEST_PAFIK, test_ikflow=TEST_IKFLOW)

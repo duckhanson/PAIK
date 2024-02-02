90eb47cdfcdc325be1812065c189aaec2e3e4d9c
chiahanlu <ducklyu0301@gmail.com>
2024-02-03 01:39:52 +0800

    batch input

--- example/exp2_following.py
+++ example/exp2_following.py
@@ -14,130 +14,49 @@ import torch
 from tqdm import trange
 from ikflow.utils import set_seed
 from ikflow.model_loading import get_ik_solver
+from jkinpylib.evaluation import solution_pose_errors
+
 
 TEST_PAFIK = True
-TEST_IKFLOW = True
-USE_NSF_ONLY = False
+TEST_IKFLOW = False
+USE_NSF_ONLY = True
 LOAD_TIME = ""  # 0131005046
-NUM_STEPS = 10
-NUM_TRAJECTORIES = 1000
+NUM_STEPS = 20
+NUM_TRAJECTORIES = 10000
 NUM_SOLS = 1
+STD = 0.25
 DDJC_THRES = (40, 80, 120)
-
-
-def path_following(test_pafik: bool, test_ikflow: bool):
-    solver_param = DEFAULT_NSF if USE_NSF_ONLY else DEFULT_SOLVER
-    solver = PathFollower(solver_param=solver_param)
-
-    # J, P = solver.sample_Jtraj_Ppath(load_time=LOAD_TIME, num_steps=NUM_STEPS)
-    J, P = solver.sample_Jtraj_Ppath_multiple_trajectories(
-        num_steps=NUM_STEPS, num_traj=NUM_TRAJECTORIES
-    )
-    print(J.shape, P.shape)
-    # if test_pafik:
-    #     begin_time = time()
-    #     J_hat, l2_errs, ang_errs, mjac_arr, ddjc = solver.solve_path(
-    #         J, P, num_traj=NUM_TRAJECTORIES, return_evaluation=True
-    #     )
-    #     end_time = time()
-
-    #     df = pd.DataFrame(
-    #         {
-    #             "l2_errs": l2_errs,
-    #             "ang_errs": np.rad2deg(ang_errs),
-    #             "mjac": mjac_arr,
-    #             "ddjc": np.rad2deg(ddjc),
-    #         }
-    #     )
-
-    #     table = np.empty((len(DDJC_THRES), 3))
-    #     for i, thres in enumerate(DDJC_THRES):
-    #         sr = df.query(f"ddjc < {thres}")["ddjc"].count() / df.shape[0]
-    #         table[i] = (thres, sr, round(1 / sr)) if sr != 0 else (thres, sr, np.inf)
-    #     print(tabulate(table, headers=["ddjc", "success rate", "min num sols"]))
-    #     print(
-    #         f"avg_inference_time: {round((end_time - begin_time) / NUM_TRAJECTORIES, 3)}"
-    #     )
-    #     print(df.describe())
-
-    # if test_ikflow:
-    #     set_seed()
-
-    #     # Build IKFlowSolver and set weights
-    #     ik_solver, _ = get_ik_solver("panda__full__lp191_5.25m")
-
-    #     F_l2_err = np.empty((NUM_TRAJECTORIES,))
-    #     F_ang_err = np.empty((NUM_TRAJECTORIES,))
-    #     F_mjac = np.empty((NUM_TRAJECTORIES,))
-    #     F_ddjc = np.empty((NUM_TRAJECTORIES,))
-    #     F_runtime = np.empty((NUM_TRAJECTORIES))
-
-    #     shape = (P.shape[0], ik_solver._network_width)
-    #     begin_time = time()
-    #     for i in trange(NUM_TRAJECTORIES):
-    #         latent = torch.from_numpy(
-    #             np.tile(np.random.randn(1, shape[1]), (shape[0], 1)).astype(np.float32)
-    #         ).to("cuda")
-    #         sol, l2, ang, _, _, runtime = ik_solver.solve_n_poses(
-    #             P, latent=latent, refine_solutions=False, return_detailed=True
-    #         )  # type: ignore
-    #         sol = sol.cpu().numpy()
-    #         F_l2_err[i] = l2.mean()
-    #         F_ang_err[i] = ang.mean()
-    #         F_mjac[i] = max_joint_angle_change(sol)  # type: ignore
-    #         F_ddjc[i] = np.linalg.norm(sol - J, axis=-1).mean()
-    #         F_runtime[i] = runtime
-    #     end_time = time()
-    #     F_df = pd.DataFrame(
-    #         {
-    #             "l2_err": F_l2_err,
-    #             "ang_err": np.rad2deg(F_ang_err),
-    #             "mjac": F_mjac,
-    #             "ddjc": np.rad2deg(F_ddjc),
-    #         }
-    #     )
-
-    #     table = np.empty((len(DDJC_THRES), 3))
-    #     for i, thres in enumerate(DDJC_THRES):
-    #         sr = F_df.query(f"ddjc < {thres}")["ddjc"].count() / F_df.shape[0]
-    #         table[i] = (thres, sr, round(1 / sr)) if sr != 0 else (thres, sr, np.inf)
-    #     print(tabulate(table, headers=["ddjc", "success rate", "min num sols"]))
-    #     print(
-    #         f"avg_inference_time: {round((end_time - begin_time) / NUM_TRAJECTORIES, 3)}"
-    #     )
-    #     print(F_df.describe())
-
 
 def path_following_multiple_trajectory(test_pafik: bool, test_ikflow: bool):
     solver_param = DEFAULT_NSF if USE_NSF_ONLY else DEFULT_SOLVER
     solver = PathFollower(solver_param=solver_param)
 
-    J, P = solver.sample_Jtraj_Ppath_multiple_trajectories(
+    Jt, Pt = solver.sample_Jtraj_Ppath_multiple_trajectories(
         num_steps=NUM_STEPS, num_traj=NUM_TRAJECTORIES
     )
 
     if test_pafik:
-        l2 = np.empty((NUM_TRAJECTORIES, NUM_SOLS))
-        ang = np.empty((NUM_TRAJECTORIES, NUM_SOLS))
-        mjac = np.empty((NUM_TRAJECTORIES, NUM_SOLS))
-        ddjc = np.empty((NUM_TRAJECTORIES, NUM_SOLS))
 
         begin_time = time()
-        for i in trange(NUM_TRAJECTORIES):
-            _, l2[i], ang[i], mjac[i], ddjc[i] = solver.solve_path(
-                np.atleast_2d(J[i]),
-                np.atleast_2d(P[i]),
-                num_sols=NUM_SOLS,
-                return_evaluation=True,
-            )
+        J = Jt.reshape(-1, Jt.shape[-1])
+        P = Pt.reshape(-1, Pt.shape[-1])
+        J_hat = solver.solve_batch(P, solver._F[solver.J_knn.kneighbors(
+            J, return_distance=False).flatten()], num_sols=1)  # type: ignore
+        l2, ang = solver.evaluate_solutions(
+            J_hat, P, return_all=True)
+        ddjc = np.linalg.norm(J_hat - J, axis=-1).reshape(NUM_TRAJECTORIES, NUM_STEPS)
+        mjac = np.array([max_joint_angle_change(qs) for qs in J_hat.reshape(NUM_TRAJECTORIES, NUM_STEPS, -1)])
+        
         avg_inference_time = round((time() - begin_time) / NUM_TRAJECTORIES, 3)
-
+        l2 = l2.reshape(NUM_TRAJECTORIES, NUM_STEPS).mean(axis=-1)
+        ang = ang.reshape(NUM_TRAJECTORIES, NUM_STEPS).mean(axis=-1)
+        ddjc = ddjc.mean(axis=-1)
         df = pd.DataFrame(
             {
-                "l2": l2.flatten(),
-                "ang": np.rad2deg(ang.flatten()),
-                "mjac": mjac.flatten(),
-                "ddjc": np.rad2deg(ddjc.flatten()),
+                "l2": l2,
+                "ang": np.rad2deg(ang),
+                "mjac": mjac,
+                "ddjc": np.rad2deg(ddjc),
             }
         )
 
@@ -159,49 +78,44 @@ def path_following_multiple_trajectory(test_pafik: bool, test_ikflow: bool):
         # Build IKFlowSolver and set weights
         ik_solver, _ = get_ik_solver("panda__full__lp191_5.25m")
 
-        F_l2_err = np.empty((NUM_TRAJECTORIES, NUM_SOLS))
-        F_ang_err = np.empty((NUM_TRAJECTORIES, NUM_SOLS))
-        F_mjac = np.empty((NUM_TRAJECTORIES, NUM_SOLS))
-        F_ddjc = np.empty((NUM_TRAJECTORIES, NUM_SOLS))
+        l2 = np.empty((NUM_TRAJECTORIES, NUM_STEPS))
+        ang = np.empty((NUM_TRAJECTORIES, NUM_STEPS))
+        mjac = np.empty((NUM_TRAJECTORIES))
+        ddjc = np.empty((NUM_TRAJECTORIES, NUM_STEPS))
 
         begin_time = time()
-        for i in trange(NUM_TRAJECTORIES):
-            for j in range(NUM_SOLS):
-                sol, l2, ang, _, _, _ = ik_solver.solve_n_poses(
-                    P[i],
-                    latent_scale=0.25,
-                    refine_solutions=False,
-                    return_detailed=True,
-                )  # type: ignore
-                sol = sol.cpu().numpy()
-                F_l2_err[i, j] = l2.mean()
-                F_ang_err[i, j] = ang.mean()
-                F_mjac[i, j] = max_joint_angle_change(sol)  # type: ignore
-                F_ddjc[i, j] = np.linalg.norm(sol - J[i], axis=-1).mean()
+        J_hat = torch.empty((NUM_TRAJECTORIES, NUM_STEPS, Jt.shape[-1]), dtype=torch.float32, device="cpu")
+        for i in trange(NUM_STEPS):
+            J_hat[:, i, :] = ik_solver.solve_n_poses(Pt[:, i], latent_scale=STD, refine_solutions=False, return_detailed=False).cpu()  # type: ignore
+            l2[:, i], ang[:, i] = solution_pose_errors(ik_solver.robot, J_hat[:, i, :], Pt[:, i])
+        
+        ddjc = np.linalg.norm(J_hat - Jt, axis=-1)
+        mjac = np.array([max_joint_angle_change(qs) for qs in J_hat])
+
         avg_inference_time = round((time() - begin_time) / NUM_TRAJECTORIES, 3)
 
-        F_df = pd.DataFrame(
+        df = pd.DataFrame(
             {
-                "l2_err": F_l2_err.flatten(),
-                "ang_err": np.rad2deg(F_ang_err.flatten()),
-                "mjac": F_mjac.flatten(),
-                "ddjc": np.rad2deg(F_ddjc.flatten()),
+                "l2": l2.mean(axis=1),
+                "ang": np.rad2deg(ang.mean(axis=1)),
+                "mjac": mjac,
+                "ddjc": np.rad2deg(ddjc.mean(axis=1)),
             }
         )
+        print(df.describe())
 
         print(
             tabulate(
                 [
                     (
                         thres,
-                        F_df.query(f"ddjc < {thres}")["ddjc"].count() / F_df.shape[0],
+                        df.query(f"ddjc < {thres}")["ddjc"].count() / df.shape[0],
                     )
                     for thres in DDJC_THRES
                 ],
                 headers=["ddjc", "success rate"],
             )
         )
-        print(F_df.describe())
         print(f"avg_inference_time: {avg_inference_time}")
 
 

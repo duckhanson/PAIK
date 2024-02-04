775367923badccb4b56bf1584a1052c86fe8fc31
chiahanlu <ducklyu0301@gmail.com>
2024-02-04 16:24:31 +0800

    U setup

--- setup.py
+++ setup.py
@@ -2,28 +2,28 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='pafik',
-    version='0.0.10',
+    version='0.1.0',
+    author='chiahanlu',
+    author_email='ducklyu0301@gmail.com',
     description="An data-driven posture-aware IK solver",
-    package_dir={"": "pafik"},
-    packages=find_packages(),
+    keywords='data-driven, inverse kinematics, posture',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/duckhanson/PAFIK",
-    author="chiahanlu",
-    author_email="ducklyu0301@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where='src'),
+    # url="https://github.com/duckhanson/PAFIK",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.9.12",
         "Operating System :: OS Independent",
     ],
-    install_requires=["numpy", "warp-lang", 'usd-core',
-                      'urdfpy', 'pytorch_lightning', 'torchdiffeq'],
-    extras_require={
-        "dev": ["pytest>=7.0", "twine>=4.0.2"],
-    },
+    install_requires=required,
     python_requires=">=3.9.12",
 )

cd45112f6f3f7b93807a0c19152a3edd830f2479
chiahanlu <ducklyu0301@gmail.com>
2024-02-15 01:45:17 +0800

    merge pafik and ikflow into single file for every exp

--- example/exp1_ikp.py
+++ example/exp1_ikp.py
@@ -1,9 +1,15 @@
 # Import required packages
+import time
 import numpy as np
 from tabulate import tabulate
 from tqdm import trange
+import torch
 from pafik.solver import Solver
 from pafik.settings import DEFAULT_NSF, DEFULT_SOLVER
+
+from ikflow.utils import set_seed
+from ikflow.model_loading import get_ik_solver
+from jkinpylib.evaluation import solution_pose_errors
 
 NUM_POSES = 3000  # 100
 NUM_SOLS = 400  # 1000
@@ -15,7 +21,7 @@ USE_NSF_ONLY = False
 METHOD_OF_SELECT_REFERENCE_POSTURE = "knn"
 
 
-def ikp():
+def pafik():
     solver_param = DEFAULT_NSF if USE_NSF_ONLY else DEFULT_SOLVER
     solver_param.workdir = WORKDIR
     solver_param.select_reference_posture_method = METHOD_OF_SELECT_REFERENCE_POSTURE
@@ -39,7 +45,46 @@ def ikp():
             ],
         )
     )
+    
+def ikflow():
+    set_seed()
+
+    # Build IKFlowSolver and set weights
+    ik_solver, _ = get_ik_solver("panda__full__lp191_5.25m")
+    _, P = ik_solver.robot.sample_joint_angles_and_poses(n=NUM_POSES)
+    l2 = np.zeros((NUM_SOLS, len(P)))
+    ang = np.zeros((NUM_SOLS, len(P)))
+    J = torch.empty((NUM_SOLS, len(P), 7), dtype=torch.float32, device="cpu")
+    begin = time.time()
+    if NUM_POSES < NUM_SOLS:
+        for i in trange(NUM_POSES):
+            J[:, i, :] = ik_solver.solve(
+                P[i],
+                n=NUM_SOLS,
+                latent_scale=STD,
+                refine_solutions=False,
+                return_detailed=False,
+            ).cpu()  # type: ignore
+
+            l2[:, i], ang[:, i] = solution_pose_errors(
+                ik_solver.robot, J[:, i, :], P[i]
+            )
+    else:
+        for i in trange(NUM_SOLS):
+            J[i] = ik_solver.solve_n_poses(
+                P, latent_scale=STD, refine_solutions=False, return_detailed=False
+            ).cpu()
+            l2[i], ang[i] = solution_pose_errors(ik_solver.robot, J[i], P)
+    avg_inference_time = round((time.time() - begin) / NUM_POSES, 3)
+
+    print(
+        tabulate(
+            [[l2.mean(), np.rad2deg(ang.mean()), avg_inference_time]],
+            headers=["avg_l2", "avg_ang", "avg_inference_time"],
+        )
+    )
 
 
 if __name__ == "__main__":
-    ikp()
+    pafik()
+    ikflow()
--- example/exp2_following.py
+++ example/exp2_following.py
@@ -8,6 +8,10 @@ from pafik.follower import PathFollower, max_joint_angle_change
 import torch
 from tqdm import trange
 
+from ikflow.utils import set_seed
+from ikflow.model_loading import get_ik_solver
+from jkinpylib.evaluation import solution_pose_errors
+
 
 WORKDIR = "/home/luca/example_package/pafik"
 TEST_PAFIK = True
@@ -21,7 +25,7 @@ STD = 0.01
 DDJC_THRES = (40, 80, 120)
 
 
-def path_following_multiple_trajectory(test_pafik: bool, test_ikflow: bool):
+def pafik():
     solver_param = DEFAULT_NSF if USE_NSF_ONLY else DEFULT_SOLVER
     solver_param.workdir = WORKDIR
     solver = PathFollower(solver_param=solver_param)
@@ -68,6 +72,65 @@ def path_following_multiple_trajectory(test_pafik: bool, test_ikflow: bool):
     print(df.describe())
     print(f"avg_inference_time: {avg_inference_time}")
 
+    
+def ikflow():
+    solver_param = DEFULT_SOLVER
+    solver_param.workdir = WORKDIR
+    solver = PathFollower(solver_param=solver_param)
+
+    Jt, Pt = solver.sample_multiple_Jtraj_and_Ppath(
+        num_steps=NUM_STEPS, num_traj=NUM_TRAJECTORIES
+    )
+
+    set_seed()
+
+    # Build IKFlowSolver and set weights
+    ik_solver, _ = get_ik_solver("panda__full__lp191_5.25m")
+
+    l2 = np.empty((NUM_TRAJECTORIES, NUM_STEPS))
+    ang = np.empty((NUM_TRAJECTORIES, NUM_STEPS))
+    mjac = np.empty((NUM_TRAJECTORIES))
+    ddjc = np.empty((NUM_TRAJECTORIES, NUM_STEPS))
+
+    begin_time = time()
+    J_hat = torch.empty(
+        (NUM_TRAJECTORIES, NUM_STEPS, Jt.shape[-1]), dtype=torch.float32, device="cpu"
+    )
+    for i in trange(NUM_STEPS):
+        J_hat[:, i, :] = ik_solver.solve_n_poses(
+            Pt[:, i], latent_scale=STD, refine_solutions=False, return_detailed=False
+        ).cpu()  # type: ignore
+        l2[:, i], ang[:, i] = solution_pose_errors(
+            ik_solver.robot, J_hat[:, i, :], Pt[:, i]
+        )
+
+    ddjc = np.linalg.norm(J_hat - Jt, axis=-1)
+    mjac = np.array([max_joint_angle_change(qs) for qs in J_hat])
+
+    avg_inference_time = round((time() - begin_time) / NUM_TRAJECTORIES, 3)
+
+    df = pd.DataFrame(
+        {
+            "l2": l2.mean(axis=1),
+            "ang": np.rad2deg(ang.mean(axis=1)),
+            "mjac": mjac,
+            "ddjc": np.rad2deg(ddjc.mean(axis=1)),
+        }
+    )
+    print(df.describe())
+
+    print(
+        tabulate(
+            [
+                (thres, df.query(f"ddjc < {thres}")["ddjc"].count() / df.shape[0])
+                for thres in DDJC_THRES
+            ],
+            headers=["ddjc", "success rate"],
+        )
+    )
+    print(f"avg_inference_time: {avg_inference_time}")
+
 
 if __name__ == "__main__":
-    path_following_multiple_trajectory()
+    pafik()
+    ikflow()

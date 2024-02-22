24d733799df4fc509880bee90cd824ee3cc023c2
chiahanlu <ducklyu0301@gmail.com>
2024-02-22 16:11:14 +0800

    Create mmd

---
+++ example/mmd_helper.py
@@ -0,0 +1,66 @@
+import torch
+from torch import nn
+
+
+class RBF(nn.Module):
+    def __init__(self, n_kernels=5, mul_factor=2.0, bandwidth=None):
+        super().__init__()
+        self.bandwidth_multipliers = mul_factor ** (
+            torch.arange(n_kernels) - n_kernels // 2
+        )
+        self.bandwidth = bandwidth
+
+    def get_bandwidth(self, L2_distances):
+        if self.bandwidth is None:
+            n_samples = L2_distances.shape[0]
+            return L2_distances.data.sum() / (n_samples**2 - n_samples)
+
+        return self.bandwidth
+
+    def forward(self, X):
+        L2_distances = torch.cdist(X, X) ** 2
+        return torch.exp(
+            -L2_distances[None, ...]
+            / (self.get_bandwidth(L2_distances) * self.bandwidth_multipliers)[
+                :, None, None
+            ]
+        ).sum(dim=0)
+
+
+class MMDLoss(nn.Module):
+    def __init__(self, kernel=RBF()):
+        super().__init__()
+        self.kernel = kernel
+
+    def forward(self, X, Y):
+        X_filtered = X[~torch.any(X.isnan(), dim=1)]
+        Y_filtered = Y[~torch.any(Y.isnan(), dim=1)]
+        K = self.kernel(torch.vstack([X_filtered, Y_filtered]))
+
+        X_size = X.shape[0]
+        XX = K[:X_size, :X_size].mean()
+        XY = K[:X_size, X_size:].mean()
+        YY = K[X_size:, X_size:].mean()
+        return XX - 2 * XY + YY
+
+
+def mmd_evaluate_multiple_poses(
+    J_hat_,
+    J_ground_truth_,
+    num_poses,
+    return_all=False,
+):
+    assert (
+        len(J_hat_) == num_poses and len(J_ground_truth_) == num_poses
+    ), f"J_hat_.shape: {J_hat_.shape}, num_poses: {num_poses}"
+    mmd_score = MMDLoss()
+    J_hat_ = torch.tensor(J_hat_, dtype=torch.float32, device="cuda")
+    J_ground_truth_ = torch.tensor(J_ground_truth_, dtype=torch.float32, device="cuda")
+    mmd_all_poses = torch.empty(num_poses, device="cuda")
+    for i in range(num_poses):
+        mmd_all_poses[i] = mmd_score(J_hat_[i], J_ground_truth_[i])
+    mmd_all_poses = mmd_all_poses.cpu().numpy()
+
+    if return_all:
+        return mmd_all_poses
+    return mmd_all_poses.mean()

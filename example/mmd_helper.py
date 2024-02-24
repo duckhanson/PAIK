1dbaeb1ecfd9ccd60942984d80fdfa216c4716f5
chiahanlu <ducklyu0301@gmail.com>
2024-02-24 19:06:44 +0800

    tidy up mmd_functions

--- example/mmd_helper.py
+++ example/mmd_helper.py
@@ -1,65 +1,69 @@
+from typing import List, Tuple
 import torch
-from torch import nn
+
+def guassian_kernel(L2_distances, n_kernels=5, mul_factor=2.0):
+    n_samples = L2_distances.shape[0]
+    bandwith = L2_distances.data.sum() / (n_samples ** 2 - n_samples)
+    bandwidth_multipliers = mul_factor ** (
+        torch.arange(n_kernels, device="cuda") - n_kernels // 2
+    )
+
+    return torch.exp(
+        -L2_distances[None, ...] / (bandwith * bandwidth_multipliers)[:, None, None]
+    ).sum(dim=0)
 
 
-class RBF(nn.Module):
-    def __init__(self, n_kernels=5, mul_factor=2.0, bandwidth=None):
-        super().__init__()
-        self.bandwidth_multipliers = mul_factor ** (
-            torch.arange(n_kernels, device="cuda") - n_kernels // 2
-        )
-
-        self.bandwidth = bandwidth
-
-    def get_bandwidth(self, L2_distances):
-        if self.bandwidth is None:
-            n_samples = L2_distances.shape[0]
-            return L2_distances.data.sum() / (n_samples ** 2 - n_samples)
-
-        return self.bandwidth
-
-    def forward(self, X):
-        L2_distances = torch.cdist(X, X) ** 2
-        # assert L2_distances.device == "cuda" and self.bandwidth_multipliers.device == "cuda" and self.get_bandwidth(L2_distances).device == "cuda", f"L2_dis: {L2_distances.device}, bandmult: {self.bandwidth_multipliers.device}, band: {self.get_bandwidth(L2_distances).device}"
-        return torch.exp(
-            -L2_distances[None, ...]
-            / (self.get_bandwidth(L2_distances) * self.bandwidth_multipliers)[
-                :, None, None
-            ]
-        ).sum(dim=0)
+def inverse_multiquadric_kernel(
+    L2_distances, widths_exponents: List[Tuple[float, float]]
+):
+    kernel_val = [
+        width ** exponent * ((width + L2_distances) / exponent) ** -exponent
+        for width, exponent in widths_exponents
+    ]
+    return sum(kernel_val)
 
 
-class MMDLoss(nn.Module):
-    def __init__(self, kernel=RBF()):
-        super().__init__()
-        self.kernel = kernel
+def mmd_score(
+    X, Y, use_inverse_multi_quadric, mmd_kernels=[(0.2, 0.1), (0.2, 0.5), (0.2, 2)]
+):
+    X = X[~torch.any(X.isnan(), dim=1)]
+    Y = Y[~torch.any(Y.isnan(), dim=1)]
 
-    def forward(self, X, Y):
-        X_filtered = X[~torch.any(X.isnan(), dim=1)]
-        Y_filtered = Y[~torch.any(Y.isnan(), dim=1)]
+    min_len = min(X.shape[0], Y.shape[0])
+    X, Y = X[:min_len], Y[:min_len]
 
-        if X_filtered.shape[0] == 0 or Y_filtered.shape[0] == 0:
-            return torch.nan
-        # print(f"X_filtered: {X_filtered.shape}, Y_filtered: {Y_filtered.shape}")
-        K = self.kernel(torch.vstack([X_filtered, Y_filtered]))
+    if min_len == 0:
+        return torch.nan
 
-        X_size = X_filtered.shape[0]
-        XX = K[:X_size, :X_size].mean()
-        XY = K[:X_size, X_size:].mean()
-        YY = K[X_size:, X_size:].mean()
-        return XX - 2 * XY + YY
+    total = torch.vstack([X, Y])
+    L2_distances = torch.cdist(total, total).square()
+
+    if use_inverse_multi_quadric:
+        kernels = inverse_multiquadric_kernel(L2_distances, mmd_kernels)
+    else:
+        kernels = guassian_kernel(L2_distances)
+
+    XX = kernels[:min_len, :min_len]
+    YY = kernels[min_len:, min_len:]
+    XY = kernels[:min_len, min_len:]
+    return (XX - 2 * XY + YY).mean()
 
 
-def mmd_evaluate_multiple_poses(J_hat_, J_ground_truth_, num_poses):
+def mmd_evaluate_multiple_poses(
+    J_hat_, J_ground_truth_, num_poses, use_inverse_multi_quadric=True
+):
     assert (
         len(J_hat_) == num_poses and len(J_ground_truth_) == num_poses
     ), f"J_hat_.shape: {J_hat_.shape}, num_poses: {num_poses}"
-    mmd_score = MMDLoss()
-    J_hat_ = torch.tensor(J_hat_, dtype=torch.float32, device="cuda")
-    J_ground_truth_ = torch.tensor(J_ground_truth_, dtype=torch.float32, device="cuda")
-    mmd_all_poses = torch.empty(num_poses, device="cuda")
-    for i in range(num_poses):
-        mmd_all_poses[i] = mmd_score(J_hat_[i], J_ground_truth_[i])
-    mmd_all_poses = mmd_all_poses[~torch.isnan(mmd_all_poses)]
-    mmd_all_poses = mmd_all_poses.cpu().numpy()
-    return mmd_all_poses.mean()
+
+    J_hat_ = torch.from_numpy(J_hat_).float().to("cuda")
+    J_ground_truth_ = torch.from_numpy(J_ground_truth_).float().to("cuda")
+    mmd_all_poses = torch.tensor(
+        [
+            mmd_score(J_hat_[i], J_ground_truth_[i], use_inverse_multi_quadric)
+            for i in range(num_poses)
+        ],
+        device="cuda",
+    )
+    mmd_mean = mmd_all_poses[~torch.isnan(mmd_all_poses)].mean()
+    return mmd_mean.item()

3212473d5ad3f1a74ec1e0d7e0890128514a7c02
chiahanlu <ducklyu0301@gmail.com>
2024-02-24 15:53:08 +0800

    black format

--- example/mmd_helper.py
+++ example/mmd_helper.py
@@ -8,12 +8,13 @@ class RBF(nn.Module):
         self.bandwidth_multipliers = mul_factor ** (
             torch.arange(n_kernels, device="cuda") - n_kernels // 2
         )
+
         self.bandwidth = bandwidth
 
     def get_bandwidth(self, L2_distances):
         if self.bandwidth is None:
             n_samples = L2_distances.shape[0]
-            return L2_distances.data.sum() / (n_samples**2 - n_samples)
+            return L2_distances.data.sum() / (n_samples ** 2 - n_samples)
 
         return self.bandwidth
 
@@ -49,11 +50,7 @@ class MMDLoss(nn.Module):
         return XX - 2 * XY + YY
 
 
-def mmd_evaluate_multiple_poses(
-    J_hat_,
-    J_ground_truth_,
-    num_poses,
-):
+def mmd_evaluate_multiple_poses(J_hat_, J_ground_truth_, num_poses):
     assert (
         len(J_hat_) == num_poses and len(J_ground_truth_) == num_poses
     ), f"J_hat_.shape: {J_hat_.shape}, num_poses: {num_poses}"

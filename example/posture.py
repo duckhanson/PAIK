2710e2017f583448173ba2c2d9a674106c8fe557
chiahanlu <ducklyu0301@gmail.com>
2024-03-04 15:54:03 +0800

    U integrate display into one ipynb

--- example/posture.py
+++
@@ -1,8 +0,0 @@
-from common.display import display_posture_all
-from common.config import ConfigPosture
-
-if __name__ == "__main__":
-    config = ConfigPosture()
-    config.date = "2024_03_02"
-
-    display_posture_all(config.record_dir, config.success_distance_thresholds)

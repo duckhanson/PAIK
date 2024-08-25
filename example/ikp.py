68165f21e39a1c21f6a27658ab5b980688d09f91
chiahanlu <ducklyu0301@gmail.com>
2024-08-26 00:47:37 +0800

    ikp functions support robot_name

--- example/ikp.py
+++ example/ikp.py
@@ -8,9 +8,9 @@ from common.config import Config_IKP
 from common.display import display_ikp, save_ikp
 
 
-def paik():
+def paik(robot_name: str):
     config = Config_IKP()
-    solver = get_solver(arch_name="paik", robot_name="atlas_waist_arm",
+    solver = get_solver(arch_name="paik", robot_name=robot_name,
                         load=True, work_dir=config.workdir)
 
     (l2, ang, avg_inference_time) = solver.random_ikp(
@@ -18,7 +18,6 @@ def paik():
         config.num_sols,
         std=config.std,
         batch_size=config.batch_size,
-        success_threshold=config.success_threshold,
         select_reference=config.select_reference,
     )  # type: ignore
 
@@ -36,7 +35,6 @@ def nsf(robot_name: str):
         config.num_sols,
         std=config.std,
         batch_size=config.batch_size,
-        success_threshold=config.success_threshold,
     )  # type: ignore
 
     save_ikp(config.record_dir, "nsf", l2, ang,
@@ -44,5 +42,6 @@ def nsf(robot_name: str):
 
 
 if __name__ == "__main__":
-    paik()
-    nsf()
+    robot_name = "fetch"
+    paik(robot_name=robot_name)
+    nsf(robot_name=robot_name)

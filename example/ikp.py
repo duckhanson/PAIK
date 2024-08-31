aaf63e73d45a0dadaba6feda61f1b292843a1072
chiahanlu <ducklyu0301@gmail.com>
2024-09-01 00:33:58 +0800

    mmd -> (num_sols, num_poses, -1)

--- example/ikp.py
+++ example/ikp.py
@@ -22,28 +22,29 @@ def numerical_inverse_kinematics_single(solver, pose, num_sols):
     return ik_sols
 
 def numerical_inverse_kinematics_batch(solver, P, num_sols):
-    # return shape: (1, num_poses*num_sols, n)
-    return np.asarray(
-        [numerical_inverse_kinematics_single(solver, p, num_sols) for p in tqdm(P)]
-    ).reshape(1, -1, solver.n)
+    ik_sols_batch = np.empty((num_sols, P.shape[0], solver.n))
+    for i, pose in enumerate(tqdm(P)):
+        ik_sols_batch[:, i] = numerical_inverse_kinematics_single(solver, pose, num_sols)
+    # return shape: (num_sols, num_poses, n)
+    return ik_sols_batch
     
 
 def solver_batch(solver, P, num_sols, std=0.001):
     solver.base_std = std
 
-    # shape: (num_poses, num_sols, m)
-    P_num_sols = np.expand_dims(P, axis=1).repeat(num_sols, axis=1)
-    # shape: (num_poses * num_sols, n)
+    # shape: (num_sols, num_poses, m)
+    P_num_sols = np.expand_dims(P, axis=0).repeat(num_sols, axis=0)
+    # shape: (num_sols*num_poses, n)
     P_num_sols = P_num_sols.reshape(-1, P.shape[-1])
     
     if isinstance(solver, PAIK):
         F =  solver.get_reference_partition_label(P=P, num_sols=num_sols)
-        # shape: (1, num_poses * num_sols, n)
+        # shape: (1, num_sols*num_poses, n)
         J_hat = solver.generate_ik_solutions(P=P_num_sols, F=F)
     else:
         J_hat = solver.generate_ik_solutions(P=P_num_sols)
-    
-    return J_hat
+    # return shape: (num_sols, num_poses, n)
+    return J_hat.reshape(num_sols, P.shape[0], -1)
 
 def random_ikp(solver: Solver, P: np.ndarray, num_sols: int, solve_fn_batch: Any, std: float=None, verbose: bool=False):
     """
@@ -58,9 +59,10 @@ def random_ikp(solver: Solver, P: np.ndarray, num_sols: int, solve_fn_batch: Any
         verbose (bool, optional): print the statistics of the generated solutions. Defaults to False.
         
     Returns:
-        np.ndarray, float, float: the generated IK solutions with shape (num_poses, num_sols, num_dofs or n).
-        float: the mean of l2 error (m).
-        float: the mean of angular error (rad).
+        np.ndarray, float, float: the generated IK solutions with shape (num_sols, num_poses, num_dofs or n).
+        float: the mean of l2 error (mm).
+        float: the mean of angular error (deg).
+        float: the average time to solve num_sols for a pose (ms).
     """
     if verbose:
         if solve_fn_batch.__name__ == "numerical_inverse_kinematics_batch":
@@ -71,35 +73,35 @@ def random_ikp(solver: Solver, P: np.ndarray, num_sols: int, solve_fn_batch: Any
     # shape: (num_poses, num_sols, num_dofs or n)
     num_poses = P.shape[0]
     begin = time()
-    if std is None:
+    if solve_fn_batch.__name__ == "numerical_inverse_kinematics_batch":
         J_hat = solve_fn_batch(solver, P, num_sols)
     else:
         J_hat = solve_fn_batch(solver, P, num_sols, std)
+    assert J_hat.shape == (num_sols, num_poses, solver.n), f"J_hat shape {J_hat.shape} is not correct"
 
     l2, ang = solver.evaluate_pose_error_J3d_P2d(
         # input J.shape = (num_sols, num_poses, num_dofs or n)
-        J_hat.reshape(num_poses, num_sols, solver.n).transpose(1, 0, 2), P, return_all=True
+        J_hat, P, return_all=True
     )
     l2_mm = l2 * 1000
     ang_deg = np.rad2deg(ang)
     solve_time_ms = (time() - begin)/num_poses *1000
-    df = pd.DataFrame({"l2 (mm)": l2_mm, "ang (deg)": ang_deg})
-    return J_hat.reshape(num_poses, num_sols, -1), l2_mm[~np.isnan(l2_mm)].mean(), ang_deg[~np.isnan(ang_deg)].mean(), solve_time_ms
+    return J_hat, l2_mm[~np.isnan(l2_mm)].mean(), ang_deg[~np.isnan(ang_deg)].mean(), solve_time_ms
 
-def mmd(J1, J2):
+def mmd(J1, J2, num_poses):
     """
     Calculate the maximum mean discrepancy between two sets of joint angles
 
     Args:
-        J1 (np.ndarray): the first set of joint angles with shape (num_poses, num_sols, num_dofs or n)
-        J2 (np.ndarray): the second set of joint angles with shape (num_poses, num_sols, num_dofs or n)
+        J1 (np.ndarray): the first set of joint angles with shape (num_sols, num_poses, num_dofs or n)
+        J2 (np.ndarray): the second set of joint angles with shape (num_sols, num_poses, num_dofs or n)
         
     Returns:
         float: the maximum mean discrepancy between the two sets of joint angles
     """
-    num_poses = len(J1)
-    if len(J2) != num_poses:
-        raise ValueError(f"The number of poses must be the same for both J1 ({J1.shape}) and J2 ({J2[i].shape})")
+    # check num_poses is the same for J1 and J2
+    if J1.shape[1] != num_poses or J2.shape[1] != num_poses:
+        raise ValueError(f"The number of poses must be the same for both J1 ({J1.shape[1]}) and J2 ({J1.shape[1]})")
     mmd_score = mmd_evaluate_multiple_poses(J1, J2, num_poses)
     return mmd_score
 
@@ -129,8 +131,8 @@ def random_ikp_with_mmd(robot_name: str, num_poses: int, num_sols: int, std: flo
     nsf_results = random_ikp(nsf_solver, P, num_sols, solver_batch, std=std, verbose=verbose)
     paik_results = random_ikp(paik_solver, P, num_sols, solver_batch, std=std, verbose=verbose)
     
-    nsf_mmd = mmd(num_results[0], nsf_results[0])
-    paik_mmd = mmd(num_results[0], paik_results[0])
+    nsf_mmd = mmd(num_results[0], nsf_results[0], num_poses)
+    paik_mmd = mmd(num_results[0], paik_results[0], num_poses)
     
 
     if verbose:
